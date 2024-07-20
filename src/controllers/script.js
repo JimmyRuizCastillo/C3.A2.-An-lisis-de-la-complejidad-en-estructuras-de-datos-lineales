3ada597355f664e18509c601feddbe091271f767
JimmyRuizCastillo <233288@ids.upchiapas.edu.mx>
2024-07-19 23:50:39 -0600

    latest version

---
+++ src/controllers/script.js
@@ -0,0 +1,139 @@
+import LinkedList from "../models/LinkedList.js"
+let list = new LinkedList()
+//let root = document.getElementById("list-bussines")
+const rute = "./controllers/bussines.json"
+//console.time("medicion 1")
+fetch(rute)
+.then(response => response.json())
+.then(data => {
+    //En esta parte unicamente se muestran los primeros 100 registros
+    /*
+    for (let x=0;x<10;x++) {
+        let name = data[x].name
+        lista.add(name)
+    }
+    */
+
+    let arrayData = [];
+        
+        console.time("LinkedList Insertion")
+        for (let i = 0; i < 10000; i++) {
+            list.add(data[i].name)
+        }
+        /*
+        data.forEach(element => {
+            list.add(element)
+        });
+        */
+        console.timeEnd("LinkedList Insertion")
+
+        //let finalTimeLindkedInsert = console.time("LinkedList Insertion") - console.timeEnd("LinkedList Insertion")
+
+        console.time("LinkedList bubbleSort")
+        list.bubbleSort()
+        console.timeEnd("LinkedList bubbleSort")
+
+        //let finalTimeLinkedBubble = console.time("LinkedList Insertion") - console.timeEnd("LinkedList Insertion")
+
+        console.time("LinkedList mergeSort")
+        list.mergeSort()
+        console.timeEnd("LinkedList mergeSort")
+
+        //let finalTimeLinkedMerge = console.time("LinkedList Insertion") - console.timeEnd("LinkedList Insertion")
+
+        console.time("LinkedList radixSort")
+        list.radixSort()
+        console.timeEnd("LinkedList radixSort")
+
+        //let finalTimeLinkedRadix = console.time("LinkedList Insertion") - console.timeEnd("LinkedList Insertion")
+
+        console.time("Array Insertion")
+        for (let i = 0; i < 10000; i++) {
+            arrayData.push(data[i].name)
+        }
+        console.timeEnd("Array Insertion")
+
+        //let finalTimeArrayInsert = console.time("LinkedList Insertion") - console.timeEnd("LinkedList Insertion")
+
+        console.time("Array bubbleSort")
+        bubbleSort(arrayData)
+        console.timeEnd("Array bubbleSort")
+
+        //let finalTimeArrayBubble = console.time("LinkedList Insertion") - console.timeEnd("LinkedList Insertion")
+
+        console.time("Array mergeSort")
+        mergeSort(arrayData)
+        console.timeEnd("Array mergeSort")
+
+        //let finalTimeArrayMerge = console.time("LinkedList Insertion") - console.timeEnd("LinkedList Insertion")
+
+        console.time("Array radixSort")
+        radixSort(arrayData)
+        console.timeEnd("Array radixSort")
+
+        //let finalTimeArrayRadix = console.time("LinkedList Insertion") - console.timeEnd("LinkedList Insertion")
+
+}).catch(err => console.log(err))
+//console.timeEnd("medicion 1")
+
+function bubbleSort(array) {
+    let len = array.length
+    for (let i = 0; i < len; i++) {
+        for (let j = 0; j < len - i - 1; j++) {
+            if (array[j] > array[j + 1]) {
+                [array[j], array[j + 1]] = [array[j + 1], array[j]]
+            }
+        }
+    }
+    return array
+}
+
+function mergeSort(array) {
+    if (array.length <= 1) return array
+
+    const mid = Math.floor(array.length / 2)
+    const left = mergeSort(array.slice(0, mid))
+    const right = mergeSort(array.slice(mid))
+
+    return merge(left, right)
+}
+
+function merge(left, right) {
+    let result = []
+    let lIndex = 0
+    let rIndex = 0
+    while (lIndex < left.length && rIndex < right.length) {
+        if (left[lIndex] < right[rIndex]) {
+            result.push(left[lIndex])
+            lIndex++
+        } else {
+            result.push(right[rIndex])
+            rIndex++
+        }
+    }
+
+    return result.concat(left.slice(lIndex)).concat(right.slice(rIndex))
+}
+
+function radixSort(array) {
+    const maxNum = Math.max(...array)
+    let digit = 1
+    while (digit <= maxNum) {
+        let buckets = [...Array(10)].map(() => [])
+        for (let num of array) {
+            buckets[Math.floor(num / digit) % 10].push(num)
+        }
+        array = [].concat(...buckets)
+        digit *= 10
+    }
+    return array
+}
+
+function searchArray(array, target) {
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] === target) {
+            return array[i]
+        }
+    }
+    return null
+}
---
+++ src/models/LinkedList.js
@@ -0,0 +1,157 @@
+import Node from "./Node.js"
+class LinkedList {
+  #head
+  count
+  constructor() {
+    this.#head = null
+    this.count = 0
+  }
+
+  add(value) {
+    let node = new Node(value)
+    if (this.#head == null) {
+      this.#head = node
+    }
+    else {
+      let current = this.#head
+      while (current.next) {
+        current = current.next
+      }
+      current.next = node
+    }
+    this.count = this.count + 1
+  }
+
+  //Ordenamiento burbuja
+  bubbleSort() {
+    if (this.#head == null) return
+
+    let swapped
+    let current
+    do {
+      swapped = false
+      current = this.#head
+
+      while (current.next != null) {
+        if (current.value > current.next.value) {
+          let temp = current.value
+          current.value = current.next.value
+          current.next.value = temp
+          swapped = true
+        }
+        current = current.next
+      }
+    } while (swapped)
+  }
+
+  //Ordenamiento merge
+  mergeSort() {
+    this.#head = this.#mergeSortRecursive(this.#head)
+  }
+
+  #mergeSortRecursive(node) {
+    if (!node || !node.next) {
+      return node
+    }
+
+    const middle = this.#getMiddle(node)
+    const secondHalf = middle.next
+    middle.next = null
+
+    const left = this.#mergeSortRecursive(node)
+    const right = this.#mergeSortRecursive(secondHalf)
+
+    return this.#mergeLists(left, right)
+  }
+
+  #getMiddle(node) {
+    if (!node) return null
+
+    let slow = node
+    let fast = node
+
+    while (fast.next && fast.next.next) {
+      slow = slow.next
+      fast = fast.next.next
+    }
+
+    return slow
+  }
+
+  #mergeLists(a, b) {
+    if (!a) return b
+    if (!b) return a
+
+    let result
+    if (a.data <= b.data) {
+      result = a
+      result.next = this.#mergeLists(a.next, b)
+    } else {
+      result = b
+      result.next = this.#mergeLists(a, b.next)
+    }
+    return result
+  }
+  //Ordenamiento Radix
+  radixSort() {
+    const max = this.#getMax()
+    for (let exp = 1; Math.floor(max / exp) > 0; exp *= 10) {
+      this.#countSort(exp)
+    }
+  }
+
+  #getMax() {
+    let max = this.#head.value
+    let current = this.#head.next
+
+    while (current) {
+      if (current.value > max) {
+        max = current.value
+      }
+      current = current.next
+    }
+    return max
+  }
+
+  #countSort(exp) {
+    const output = new Array(this.size)
+    const count = new Array(10).fill(0)
+
+    let current = this.#head
+    while (current) {
+      count[Math.floor(current.value / exp) % 10]++
+      current = current.next
+    }
+
+    for (let i = 1; i < 10; i++) {
+      count[i] += count[i - 1]
+    }
+
+    current = this.#head
+    while (current) {
+      output[count[Math.floor(current.value / exp) % 10] - 1] = current.value
+      count[Math.floor(current.value / exp) % 10]--
+      current = current.next
+    }
+
+    current = this.#head
+    for (let i = 0; i < this.size; i++) {
+      current.value = output[i]
+      current = current.next
+    }
+  }
+
+  //Buscar nodo
+  searchLinkedList(target) {
+    let current = this.#head
+    while (current) {
+      if (current.value === target) {
+        return current.value
+      }
+      current = current.next
+    }
+    return null
+  }
+}
+
+export default LinkedList
